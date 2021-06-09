d58ca57c4de884263d80b34a5b6f28a18ffd38fc
zengpengkindle <463478044@qq.com>
2021-06-09 14:45:31 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Blibli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string path = Directory.GetCurrentDirectory();
+
+            var vs = Directory.GetDirectories(path);
+            var files = new string[] { };
+            foreach (var item in vs)
+            {
+                var twodir = Directory.GetDirectories(item);
+                var inintFIle = Directory.GetFiles(item).Where(x => Path.GetExtension(x) == ".ini").FirstOrDefault();
+                if (inintFIle == null)
+                    continue;
+                var initStr = File.ReadAllLines(inintFIle, Encoding.GetEncoding("GB2312"));
+                foreach (var str in initStr)
+                {
+
+                    if (str.Contains("=") && str.Split('=')[0] == "InfoTip")
+                    {
+                        var wremove = (str.Split('=')[1].Replace("/", "-")).Replace("\\", "-");
+                        var s = Path.GetFileNameWithoutExtension(item);
+                        var targetFile = item.Replace(s, wremove);
+
+                        if (item != targetFile)
+                            Directory.Move(item, targetFile);
+                        var allDirectory = Directory.GetDirectories(targetFile);
+                        foreach (var childdir in allDirectory)
+                        {
+
+                            var InfoFIle = Directory.GetFiles(childdir).Where(x => Path.GetExtension(x) == ".info").FirstOrDefault();
+                            var renameFile = Directory.GetFiles(childdir).Where(x => Path.GetExtension(x) == ".mp4").FirstOrDefault();
+                            if (string.IsNullOrEmpty(renameFile))
+                                continue;
+                            var vdeosMode = JsonConvert.DeserializeObject<VdeosMode>(File.ReadAllText(InfoFIle, Encoding.GetEncoding("utf-8")));
+                            var oldName = Path.GetFileNameWithoutExtension(renameFile);
+                            File.Move(renameFile, childdir + "_"+ vdeosMode.PartName+ ".mp4");
+                            Directory.Delete(childdir,true);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(",", vs));
+            Console.ReadKey();
+        }
+        public static void ListFiles(FileSystemInfo info)
+        {
+            if (!info.Exists) return;
+
+            DirectoryInfo dir = info as DirectoryInfo;
+            //不是目录
+            if (dir == null) return;
+
+            FileSystemInfo[] files = dir.GetFileSystemInfos();
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = files[i] as FileInfo;
+                //是文件
+                if (file != null)
+                    Console.WriteLine(file.FullName + "\t " + file.Length);
+                //对于子目录，进行递归调用
+                else
+                    ListFiles(files[i]);
+            }
+        }
+    }
+}
---
+++ VdeosMode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blibli
+{
+    public class VdeosMode
+    {
+        public int Type { get; set; }
+        public string Aid { get; set; }
+        public string Bid { get; set; }
+        public string Cid { get; set; }
+        public object SeasonId { get; set; }
+        public object EpisodeId { get; set; }
+        public string Title { get; set; }
+        public string Uploader { get; set; }
+        public string Description { get; set; }
+        public string CoverURL { get; set; }
+        public object Tag { get; set; }
+        public string From { get; set; }
+        public string PartNo { get; set; }
+        public string PartName { get; set; }
+        public int Format { get; set; }
+        public int TotalParts { get; set; }
+        public int DownloadTimeRelative { get; set; }
+        public string CreateDate { get; set; }
+        public string TotalTime { get; set; }
+        public int[] PartTime { get; set; }
+        public int TotalSizeByte { get; set; }
+        public object TotalSizeString { get; set; }
+        public bool IsSinglePart { get; set; }
+        public bool IsDash { get; set; }
+        public bool IsMerged { get; set; }
+        public Videoinfo VideoInfo { get; set; }
+        public Audioinfo[] AudioInfo { get; set; }
+    }
+
+    public class Videoinfo
+    {
+        public int MediaType { get; set; }
+        public int CodecId { get; set; }
+        public string CodecName { get; set; }
+        public int VideoWidth { get; set; }
+        public int VideoHeight { get; set; }
+        public int Bandwidth { get; set; }
+        public float FrameRate { get; set; }
+    }
+
+    public class Audioinfo
+    {
+        public int MediaType { get; set; }
+        public int CodecId { get; set; }
+        public string CodecName { get; set; }
+        public int VideoWidth { get; set; }
+        public int VideoHeight { get; set; }
+        public int Bandwidth { get; set; }
+        public float FrameRate { get; set; }
+    }
+
+  
+}
